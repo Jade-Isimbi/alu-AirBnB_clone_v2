92f9a76f4f17f3aed774062f6957f6d9cd2681ef
Jade-Isimbi <156232169+Jade-Isimbi@users.noreply.github.com>
2024-10-11 14:50:45 +0200

    t1

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,16 +4,14 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Displays a html page with states and cities"""
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
-
+    """Displays a HTML page with states and cities"""
+        states = storage.all(State)
+        return render_template('8-cities_by_states.html', states=states)
 
 @app.teardown_appcontext
 def teardown(exception):
@@ -22,4 +20,4 @@ def teardown(exception):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
