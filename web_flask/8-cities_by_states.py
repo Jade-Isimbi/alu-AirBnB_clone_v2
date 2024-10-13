f16ea4b4ebfd8ad16e3c877c2f209128f5de5c5a
fniyonshuti <f.niyonshut1@alustudent.com>
2024-10-13 17:52:14 +0200

    HBNB filters

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""Module that starts a Flask web application"""
+
+"""Script that starts a Flask web apllication"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Displays a html page with states and cities"""
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
+    """loads all cities of a State"""
+
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    """Closes the session"""
+def teardown(self):
+    """closes the current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
